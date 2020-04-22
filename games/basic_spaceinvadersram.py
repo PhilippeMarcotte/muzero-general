690513eb1affa6a9158ef89914fc7e4f6a08349f
Philippe Marcotte <phlip.marcotte@gmail.com>
2020-04-22 10:46:11 -0400

    basic spaceinvaders config

--- games/basic_spaceinvadersram.py
+++ games/basic_spaceinvadersram.py
@@ -12,20 +12,22 @@ class MuZeroConfig:
     def __init__(self):
         self.seed = 0  # Seed for numpy, torch and the game
 
+
+
         ### Game
-        self.observation_shape = (1, 1,
-                                  128)  # Dimensions of the game observation, must be 3D. For a 1D array, please reshape it to (1, 1, length of array)
+        self.observation_shape = (1, 1, 128)  # Dimensions of the game observation, must be 3D (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
         self.action_space = [i for i in range(6)]  # Fixed list of all possible actions. You should only edit the length
         self.players = [i for i in range(1)]  # List of players. You should only edit the length
-        self.stacked_observations = 0  # Number of previous observation to add to the current observation
+        self.stacked_observations = 10  # Number of previous observations and previous actions to add to the current observation
+
+
 
         ### Self-Play
-        self.num_actors = 1  # Number of simultaneous threads self-playing to feed the replay buffer
-        self.max_moves = 27000  # Maximum number of moves if game is not finished before
+        self.num_actors = 2  # Number of simultaneous threads self-playing to feed the replay buffer
+        self.max_moves = 50000  # Maximum number of moves if game is not finished before
         self.num_simulations = 50  # Number of future moves self-simulated
         self.discount = 0.997  # Chronological discount of the reward
-        self.temperature_threshold = 10000  # Number of moves before dropping temperature to 0 (ie playing according to the max)
-        self.self_play_delay = 0  # Number of seconds to wait after each played game to adjust the self play / training ratio to avoid over/underfitting
+        self.temperature_threshold = 0  # Number of moves before dropping temperature to 0 (ie playing according to the max)
 
         # Root prior exploration noise
         self.root_dirichlet_alpha = 0.25
@@ -35,50 +37,67 @@ class MuZeroConfig:
         self.pb_c_base = 19652
         self.pb_c_init = 1.25
 
+
+
         ### Network
         self.network = "fullyconnected"  # "resnet" / "fullyconnected"
         self.support_size = 10  # Value and reward are scaled (with almost sqrt) and encoded on a vector with a range of -support_size to support_size
 
         # Residual Network
+        self.downsample = True  # Downsample observations before representation network (See paper appendix Network Architecture)
         self.blocks = 1  # Number of blocks in the ResNet
-        self.channels = 2  # Number of channels in the ResNet
-        self.reduced_channels = 2  # Number of channels before heads of dynamic and prediction networks
-        self.fc_reward_layers = []  # Define the hidden layers in the reward head of the dynamic network
-        self.fc_value_layers = []  # Define the hidden layers in the value head of the prediction network
-        self.fc_policy_layers = []  # Define the hidden layers in the policy head of the prediction network
+        self.channels = 128  # Number of channels in the ResNet
+        self.reduced_channels = 16  # Number of channels before heads of dynamic and prediction networks
+        self.resnet_fc_reward_layers = [16]  # Define the hidden layers in the reward head of the dynamic network
+        self.resnet_fc_value_layers = [16]  # Define the hidden layers in the value head of the prediction network
+        self.resnet_fc_policy_layers = [16]  # Define the hidden layers in the policy head of the prediction network
 
         # Fully Connected Network
-        self.encoding_size = 8
-        self.fc_reward_layers = [16]  # Define the hidden layers in the reward network
-        self.fc_value_layers = []  # Define the hidden layers in the value network
-        self.fc_policy_layers = []  # Define the hidden layers in the policy network
-        self.fc_representation_layers = []  # Define the hidden layers in the representation network
-        self.fc_dynamics_layers = [16]  # Define the hidden layers in the dynamics network
+        self.encoding_size = 32
+        self.fc_reward_layers = [32, 32, 8, 8]  # Define the hidden layers in the reward network
+        self.fc_value_layers = [32, 32, 8, 8]  # Define the hidden layers in the value network
+        self.fc_policy_layers = [32, 32, 8, 8]  # Define the hidden layers in the policy network
+        self.fc_representation_layers = [128, 64, 64]  # Define the hidden layers in the representation network
+        self.fc_dynamics_layers = [32, 32, 8, 8]  # Define the hidden layers in the dynamics network
+
+
 
         ### Training
-        self.results_path = os.path.join(os.path.dirname(__file__), "../results", os.path.basename(__file__)[:-3],
-                                         datetime.datetime.now().strftime(
-                                             "%Y-%m-%d--%H-%M-%S"))  # Path to store the model weights and TensorBoard logs
-        self.training_steps = 1000e3  # Total number of training steps (ie weights update according to a batch)
-        self.batch_size = 1024  # Number of parts of games to train on at each training step
-        self.num_unroll_steps = 5  # Number of game moves to keep for every batch element
-        self.checkpoint_interval = 1e3  # Number of training steps before using the model for sef-playing
-        self.window_size = 1e6  # Number of self-play games to keep in the replay buffer
-        self.td_steps = 10  # Number of steps in the future to take into account for calculating the target value
-        self.training_delay = 0  # Number of seconds to wait after each training to adjust the self play / training ratio to avoid over/underfitting
+        self.results_path = os.path.join(os.path.dirname(__file__), "../results", os.path.basename(__file__)[:-3], datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S"))  # Path to store the model weights and TensorBoard logs
+        self.training_steps = 1000000  # Total number of training steps (ie weights update according to a batch)
+        self.batch_size = 128  # Number of parts of games to train on at each training step
+        self.checkpoint_interval = 10  # Number of training steps before using the model for sef-playing
         self.value_loss_weight = 1  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
         self.training_device = "cuda" if torch.cuda.is_available() else "cpu"  # Train on GPU if available
 
+        self.optimizer = "Adam"  # "Adam" or "SGD". Paper uses SGD
         self.weight_decay = 1e-4  # L2 weights regularization
-        self.momentum = 0.9
+        self.momentum = 0.9  # Used only if optimizer is SGD
 
         # Exponential learning rate schedule
         self.lr_init = 0.05  # Initial learning rate
-        self.lr_decay_rate = 0.1  # Set it to 1 to use a constant learning rate
-        self.lr_decay_steps = 350e3
+        self.lr_decay_rate = 0.9  # Set it to 1 to use a constant learning rate
+        self.lr_decay_steps = 1000
 
-        ### Test
-        self.test_episodes = 2  # Number of games rendered when calling the MuZero test method
+        # Muzero Reanalyze
+        self.policy_update_rate = 0
+
+        ### Replay Buffer
+        self.window_size = 1000  # Number of self-play games to keep in the replay buffer
+        self.num_unroll_steps = 10  # Number of game moves to keep for every batch element
+        self.td_steps = 50  # Number of steps in the future to take into account for calculating the target value
+        self.use_last_model_value = False  # Use the last model to provide a fresher, stable n-step value (See paper appendix Reanalyze)
+
+        # Prioritized Replay (See paper appendix Training)
+        self.PER = True  # Select in priority the elements in the replay buffer which are unexpected for the network
+        self.use_max_priority = True  # Use the n-step TD error as initial priority. Better for large replay buffer
+        self.PER_alpha = 0.5  # How much prioritization is used, 0 corresponding to the uniform case, paper suggests 1
+        self.PER_beta = 1.0
+
+        ### Adjust the self play / training ratio to avoid over/underfitting
+        self.self_play_delay = 0  # Number of seconds to wait after each played game
+        self.training_delay = 0  # Number of seconds to wait after each training step
+        self.ratio = None  # Desired self played games per training step ratio. Equivalent to a synchronous version, training can take much longer. Set it to None to disable it
 
     def visit_softmax_temperature_fn(self, trained_steps):
         """
@@ -88,9 +107,9 @@ class MuZeroConfig:
         Returns:
             Positive float.
         """
-        if trained_steps < 500e3:
+        if trained_steps < 0.5 * self.training_steps:
             return 1.0
-        elif trained_steps < 750e3:
+        elif trained_steps < 0.75 * self.training_steps:
             return 0.5
         else:
             return 0.25
@@ -102,7 +121,7 @@ class Game(AbstractGame):
     """
 
     def __init__(self, seed=None):
-        self.env = gym.make("SpaceInvaders-ram-v0")
+        self.env = gym.make("SpaceInvaders-ram-v4")
         if seed is not None:
             self.env.seed(seed)
 
@@ -163,7 +182,7 @@ class Game(AbstractGame):
         self.env.render()
         input("Press enter to take a step ")
 
-    def human_action(self):
+    def human_to_action(self):
         """
         For multiplayer games, ask the user for a legal action
         and return the corresponding action number.
@@ -172,6 +191,18 @@ class Game(AbstractGame):
             An integer from the action space.
         """
         pass
+
+    def action_to_string(self, action_number):
+        """
+        Convert an action number to a string representing the action.
+
+        Args:
+            action_number: an integer from the action space.
+
+        Returns:
+            String representing the action.
+        """
+        return str(action_number)
 
     def print_action(self, action_number):
         """
